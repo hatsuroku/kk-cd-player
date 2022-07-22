1420b3197c4c8acafb2958d010982eb75e9f021d
ShellBin <maoxiaobing@live.com>
2022-07-23 00:11:45 +0800

    增加了背景和鼠标指针样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import '@styles/app.scss';
+const waveArr = ["wave1", "wave2", "wave3"];
+const waves = waveArr.map(item => require(`@assets/waves/${item}.svg`));
 
 function App() {
-    return <div>你好</div>;
+    return (
+        <div id="content">
+            <div id="pointer" />
+        </div>
+    );
 }
 
 export default App;
