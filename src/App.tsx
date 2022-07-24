8b0cae16a522008873a3230f0c463f3128f0b191
ShellBin <maoxiaobing@live.com>
2022-07-24 13:54:09 +0800

    鼠标指针方案 1 完成开发

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import '@styles/app.scss';
 
 import Background from '@/components/Background';
 import Title from '@/components/title';
-import Cursor from '@/widgets/cursor';
+import Cursor from '@/widgets/Cursor';
 
 function App() {
     return (
-        <>
+        <div onContextMenu={ e => e.preventDefault() }>
             <Background playing={true} />
             <Cursor byScript={false} positionX={0} positionY={0} />
             <Title />
-        </>
+        </div>
     );
 }
 
--- src/widgets/cursor.tsx
+++ src/widgets/cursor.tsx
@@ -1,16 +1,42 @@
-import React, {MouseEventHandler} from 'react';
+import React, { FC, useRef,useState, useEffect } from 'react';
 import classNames from "classnames";
 
 interface CursorPosition {
     byScript: boolean;
     positionX: number;
     positionY: number;
-    onClick?: MouseEventHandler;
 }
 
-const Cursor: React.FC<CursorPosition> = ({ byScript, positionX, positionY, onClick }: CursorPosition) => {
+const Cursor: FC<CursorPosition> = ({ byScript, positionX, positionY }: CursorPosition) => {
+    const cursor = useRef<HTMLInputElement>(null);
+    const [onPoke, setOnPoke] = useState(false);
+
+    function handleMouseMove(e: any) {
+        requestAnimationFrame(() => {
+            if (cursor.current) cursor.current.style.transform = `translate(${e.clientX - 5}px,${e.clientY - 5}px)`
+        })
+    }
+    function handleMouseDown(e: any) {
+        if (cursor.current && e.button == 0) setOnPoke(true);
+    }
+    function handleMouseUp(e: any) {
+        if (cursor.current && e.button == 0) setOnPoke(false);
+    }
+
+    useEffect(() => {
+        window.addEventListener('mousemove', handleMouseMove);
+        window.addEventListener('mousedown', handleMouseDown);
+        window.addEventListener('mouseup', handleMouseUp);
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+            window.removeEventListener('mousedown', handleMouseDown);
+            window.removeEventListener('mouseup', handleMouseUp);
+        }
+    }, [])
+
     return (
-        <div className={classNames('kkcdplayer-cursor','kkcdplayer-cursor-normal')} />
+        <div className={ classNames('kkcdplayer-cursor', onPoke ? 'kkcdplayer-cursor-poke' : '') }
+             ref={ cursor }/>
     );
 };
 
